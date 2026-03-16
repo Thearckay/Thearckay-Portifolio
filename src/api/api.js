c0d55fc1aa1f1f7c6b101447a463f7cf254df673
thearckay <thearckay@gmail.com>
2026-03-16 16:34:02 -0300

    Configurando para deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080'
+const API_BASE_URL = 'https://thearckay-portifolio-backend.onrender.com'
 const getHeaders = () => {
     const token = localStorage.getItem('token')
     return {
